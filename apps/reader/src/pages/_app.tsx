4a944a3d926f0f8b0807a0e605338de5a2766c3d
Polina Migel <p.ooooh@yandex.ru>
2024-10-08 17:40:56 +0300

    Do not log result of registering sw.js file

--- apps/reader/src/pages/_app.tsx
+++ apps/reader/src/pages/_app.tsx
@@ -12,17 +12,7 @@ import { Layout, Theme } from '../components'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register(`${process.env.NEXT_PUBLIC_BASE_READER_PATH}/sw.js`)
-          .then(registration => {
-            console.log('SW зарегистрирован: ', registration);
-          })
-          .catch(error => {
-            console.error('Ошибка регистрации SW: ', error);
-          })
-      })
-    }
+    if ('serviceWorker' in navigator) if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register(`${process.env.NEXT_PUBLIC_BASE_READER_PATH}/sw.js`))
   }, [])
 
   const router = useRouter()
